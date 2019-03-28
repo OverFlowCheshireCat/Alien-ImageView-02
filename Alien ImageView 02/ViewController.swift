a3f5201edd2430ebfbcc24e749fd37d600f642e8
dit03 <dit@imac-4.local>
2019-03-28 14:20:49 +0900

    Initial Commit

---
+++ Alien ImageView 02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alien ImageView 02
+//
+//  Created by dit03 on 2019. 3. 28..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
