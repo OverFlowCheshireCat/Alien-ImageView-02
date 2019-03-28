7feaf2817bea1a7292a29644e21a8ec3e8aec39b
dit03 <dit@imac-4.local>
2019-03-28 16:10:20 +0900

    Update Source

--- Alien ImageView 02/ViewController.swift
+++ Alien ImageView 02/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var countLbl: UILabel!
+    
+    var check = false
+    var count = 1
+    var timerM = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imageView.image = UIImage(named: "frame1.png")
     }
-
-
+    
+    @IBAction func updateBtn(_ sender: Any) {
+        if (!check) {
+            if (count == 5) {
+                check = true;
+                count -= 1;
+            } else {
+                count += 1;
+            }
+        } else {
+            if (count == 1) {
+                check = false;
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+        
+        imageView.image = UIImage(named: "frame" + String(count) + ".png")
+        countLbl.text = String(count) + " 번 사진"
+    }
+    
+    func autoChange() {
+        if (!check) {
+            if (count == 5) {
+                check = true;
+                count -= 1;
+            } else {
+                count += 1;
+            }
+        } else {
+            if (count == 1) {
+                check = false;
+                count += 1;
+            } else {
+                count -= 1;
+            }
+        }
+        
+        imageView.image = UIImage(named: "frame" + String(count) + ".png")
+        countLbl.text = String(count) + " 번 사진"
+    }
+    
+    @IBAction func autoBtn(_ sender: Any) {
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+            self.autoChange()
+        })
+    }
 }
-
